0c2a0986d54ae0f803039746f0e741b629d6b5fa
Saurabh Sejpal <sejpalsaurabh@users.noreply.github.com>
2016-03-02 12:03:58 +0530

    Example Class Renamed

--- postmark/src/test/java/com/postmark/java/client/PostmarkRuleClientExample.java
+++ postmark/src/test/java/com/postmark/java/client/PostmarkRuleClientExample.java
@@ -1,15 +1,42 @@
+/*
+
+    The MIT License (MIT)
+
+    Copyright (c) 2016 Saurabh Sejpal
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+    documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial
+    portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+
+*/
+
 package com.postmark.java.client;
 
-import com.postmark.java.client.triggers.rules.PostmarkRuleClient;
+import com.postmark.java.client.triggers.rule.PostmarkRuleClient;
 import com.postmark.java.exception.PostmarkException;
 import com.postmark.java.model.InboundRule;
 
 import java.util.List;
 
 /**
- * Created by Saurabh Sejpal on Feb 20 2016.
+ *
+ * This class will demonstrate functionality of Postmark Inbound Rule API.
+ *
+ * @author Saurabh Sejpal
+ * @version 1.0
+ * @since 1.0
  */
-public class PostmarkRuleClientTest {
+public class PostmarkRuleClientExample {
 
     public static void main(String[] args) throws PostmarkException {
         PostmarkRuleClient postmarkRuleClient = new PostmarkRuleClient();
--- postmark/src/test/java/com/postmark/java/client/PostmarkServerClientExample.java
+++ postmark/src/test/java/com/postmark/java/client/PostmarkServerClientExample.java
@@ -1,3 +1,25 @@
+/*
+
+    The MIT License (MIT)
+
+    Copyright (c) 2016 Saurabh Sejpal
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+    documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial
+    portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+
+*/
+
 package com.postmark.java.client;
 
 
@@ -9,9 +31,14 @@ import com.postmark.java.model.Server;
 import java.util.List;
 
 /**
- * Created by Saurabh Sejpal on Feb 21 2016.
+ *
+ * This class will demonstrate functionality of Postmark Server API.
+ *
+ * @author Saurabh Sejpal
+ * @version 1.0
+ * @since 1.0
  */
-public class PostmarkServerClientTest {
+public class PostmarkServerClientExample {
 
     public static void main(String[] args) throws PostmarkException {
         PostmarkServerClient postmarkServerClient = new PostmarkServerClient();
